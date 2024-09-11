3bbdbe25dd7ee4ec50619a010eeaeeb8f3a0878b
jithin1510 <jithinkalyan@gmail.com>
2024-09-11 13:07:10 +0530

    Updated resume

--- src/data/constants.js
+++ src/data/constants.js
@@ -8,7 +8,7 @@ export const Bio = {
       "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
     github: "https://github.com/jithin1510",
     resume:
-      "https://drive.google.com/file/d/1RroDIitkdhMFTnUV7vvIVJ-6FQYz7DTz/view?usp=sharing",
+      "https://drive.google.com/file/d/1PJWwPSSNtisqPSZ9mfJzdcdNGmlX4x58/view?usp=sharing",
     linkedin: "https://www.linkedin.com/in/jithin-kalyan-tadaka-233152229",
     insta: "https://www.instagram.com/ig_jithinkalyan/",
     facebook: "https://www.facebook.com/jithinkalyan.tadaka",
